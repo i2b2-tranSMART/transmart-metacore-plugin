3c51659bab847ef8fac0f3550b1be20c3c9efc7a
Burt Beckwith <burt@burtbeckwith.com>
2018-03-02 15:59:01 -0500

    bumped version to 18.1-SNAPSHOT

--- TransmartMetacorePluginGrailsPlugin.groovy
+++ TransmartMetacorePluginGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class TransmartMetacorePluginGrailsPlugin {
-	def version = '16.2'
+	def version = '18.1-SNAPSHOT'
 	def grailsVersion = '2.3 > *'
 	def title = 'Transmart Metacore Plugin'
 	def author = 'Valeria Hohlova'
