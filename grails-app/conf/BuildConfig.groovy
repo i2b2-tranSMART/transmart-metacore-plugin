1b16c68b84bd186edc3f594e7762700aca99342f
Burt Beckwith <burt@burtbeckwith.com>
2018-04-12 15:17:02 -0400

    upgraded to Grails 2.5

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -7,9 +7,13 @@ grails.project.dependency.resolution = {
 	log 'warn'
 
 	repositories {
-		mavenLocal() // Note: use 'grails maven-install' to install required plugins locally
+		mavenLocal()
 		grailsCentral()
 		mavenCentral()
+		mavenRepo 'http://ec2-35-170-59-132.compute-1.amazonaws.com:8080/artifactory/libs-snapshots'
+		mavenRepo 'http://ec2-35-170-59-132.compute-1.amazonaws.com:8080/artifactory/libs-releases'
+		mavenRepo 'http://ec2-35-170-59-132.compute-1.amazonaws.com:8080/artifactory/plugins-releases'
+		mavenRepo 'http://ec2-35-170-59-132.compute-1.amazonaws.com:8080/artifactory/plugins-snapshots'
 		mavenRepo 'https://repo.transmartfoundation.org/content/repositories/public/'
 	}
 
@@ -19,9 +23,12 @@ grails.project.dependency.resolution = {
 		}
 		compile 'org.apache.httpcomponents:httpclient:4.4.1'
 		compile 'net.sf.opencsv:opencsv:2.3'
+		test 'org.grails:grails-datastore-test-support:1.0.2-grails-2.4'
 	}
 
 	plugins {
+		compile ':transmart-shared:18.1-SNAPSHOT'
+
 		build ':release:3.1.2', ':rest-client-builder:2.1.1', {
 			export = false
 		}
